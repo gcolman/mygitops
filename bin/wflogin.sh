69a2321c2d44b56ddead983f04dca83bb1613744
gcolman <graeme.colman@appvia.io>
2023-02-24 09:59:26 +0000

    more

--- bin/wflogin.sh
+++ bin/wflogin.sh
@@ -20,7 +20,7 @@ TIME_USED=0
 TIMEOUT=180
 while [[ "${SUCCESS}" == "false" && $TIME_USED -lt ${TIMEOUT} ]]; do
   set +euo pipefail
-  wf login ${PROFILE} -a ${WF_API_URL} --force -u ${WF_ADMIN_USER} -p ${WF_ADMIN_PASS} --non-interactive >/dev/null 2>&1
+  wf login ${PROFILE} -a ${WF_API_URL} --force -u ${WF_ADMIN_USER} -p ${WF_ADMIN_PASS} --non-interactive --verbose>/dev/null 2>&1
   if [ $? = 0 ]; then
     SUCCESS="true"
   fi
